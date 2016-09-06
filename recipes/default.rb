354e7c74fd46f28c35749f40df59e5cc445162c1
EC2 Default User <ec2-user@ip-172-31-51-231.ec2.internal>
2016-09-06 23:23:22 +0000

    added initial files

---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: setup
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+package "git"
+
+
---
+++ recipes/setup.rb
@@ -0,0 +1,8 @@
+package 'emacs'
+package 'tree'
+
+file "/etc/motd" do
+  content "Carpe Deim, SIEZE THE CARP!"
+end
+
+
